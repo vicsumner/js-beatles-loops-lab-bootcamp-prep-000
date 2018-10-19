4bb499027ffd43e434d2dc8513ae9d4f2f1f05e4
Victoria L Sumner <vicsumner@gmail.com>
2018-10-19 21:12:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function theBeatlesPlay(musicians, instruments){
   var newArray = [];
   
   for(var i=0; i< musicians.length; i++){
-    
     newArray.push(musicians[i]+ " plays "+instruments[i]);
-   
   }
    return newArray;
 }
 
 function johnLennonFacts(facts){
   
-  while(var i=0; i<facts.length; i++){}
+  while(var i=0; i<facts.length; i++){
+    facts[i].push(+" !!!")
+  }
+  johnLennonFacts()
 }
-return
