310cfc52b0c9e43d199bb1cadd7b9f0c80fc1c86
Victoria L Sumner <vicsumner@gmail.com>
2018-10-19 21:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts){
-  var jlFacts =[];
+  var jlFacts = [];
   
   while(var i=0; i<facts.length; i++){
     jlFacts.push(facts[i] +" !!!")
